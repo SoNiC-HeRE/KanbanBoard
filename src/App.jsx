a02abff822ee18fc2a5922998f8696ce8dd1d3db
Sriyansh Shivam <sriyanshshivam1@gmail.com>
2024-03-11 23:35:35 +0530

    Adds project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { KanbanBoard } from './components/KanbanBoard';
+
+
+function App() {
+
+  return (
+    <>
+    <KanbanBoard />
+    </>
+  )
+}
+
+export default App
